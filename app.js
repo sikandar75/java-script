b247485d153afd64ebe5f91c0c2a2b79a254d712
sikandar75 <61728566+sikandar75@users.noreply.github.com>
2020-03-03 13:30:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+// var cities = [prompt("City Name 1")]
+
+
+// console.log("cities", cities)
+
+
+
+
+//-----Get input through Arry -------
+
+// var pro = prompt("enter city name")
+// var ary = []
+// var ad  = ary.push(pro)
+
+// console.log('city',ary)
+
+
+
+
+//---Get Multiple input in Arry-----
+ 
+// var ary = []
+// for(var i=0; i<5; i++)
+
+// {
+//     var pro = prompt("enter city name")
+//     var add = ary.push(pro)
+
+
+// }
+
+// console.log('city',ary)
