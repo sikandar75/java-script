26bbbedf0e6fb4b60021aa5875dea4647ec50678
sikandar75 <61728566+sikandar75@users.noreply.github.com>
2020-03-05 16:29:53 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,29 @@
-var num1=+prompt("Enter your Math marks");
-var num2=+prompt("Enter your Physics marks");
-var num3=+prompt("Enter your English marks");
-var num4=+prompt("Enter your Urdu marks");
-var num5=+prompt("Enter your Islamiat marks");
-var Obtainmarks=num1+num2+num3+num4+num5;
-var per=Obtainmarks/425*100;
-
-console.log("Math marks:" + num1);
-console.log("Physics marks:" + num2);
-console.log("English marks:" + num3);
-console.log("Urdu marks:" + num4);
-console.log("Islamiat marks:" + num5);
-console.log("Obtain Marks:"+ Obtainmarks);
-console.log("percentage:" + per + "%");
-
-if(per>=80)
-{
-   console.log("Grade:" + "A+");
-}
-else if(per>=70 && per<80)
-{
-    console.log("Grade:" + "A");
-}
-else if(per>=60 && per<70)
-{
-    console.log("Grade:" + "B");
-}
-else if(per>=50 && per<60)
-{
-    console.log("Grade:" + "C");
-}
-else if(per<50)
-{
-    console.log("Grade:" + "Fail");
-}
-else
-{
-    console.log("Invalid Grade");
-}
+//var countries=["Pakistan","India","England","Srilanka","Nepal"];
+//var country=prompt("Enter your country name");
+//var matchFound=false;
+//for(var i=0; i<=4; i++)
+//{
+  //  if(country===countries[i])
+   // {
+    //    matchFound=true;
+    //    console.log(country);
+    //}
+//}
+//if(matchFound===false)
+//{
+ //   console.log("Your country is not in the list");
+//}
+var num1=+prompt("Enter your first number");
+var num2=+prompt("Enter your second number");
+var operator1=prompt("Add Operator");
+var operator2=prompt("Sub Operator");
+var operator3=prompt("Multiply Operator");
+var operator4=prompt("Division Operator");
+var c=num1 + num2;
+var d=num1 - num2;
+var e=num1 * num2;
+var f=num1/num2;
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
